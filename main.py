9c6d2bad99154f8b65a0713b5c6b319b498e5987
H. V <hugo.vdbm@icloud.com>
2020-05-31 13:24:25 -0400

    contributors test

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ def write_txt(songs):
     		line = '=HYPERLINK("https://youtube.com' + song.get('href') + '"; "' + songName + '") \n'
     		file.write(line)
 	file.close()
+	print("playlist.txt has been generated \n")
 
 def find_playlist_info(url):	 
-	
 	#open with GET method 
 	resp=requests.get(url) 
 	
 	#http_respone 200 means OK status 
 	if resp.status_code==200: 
-		print("Successfully opened the web page") 
+		print("Successfully opened the web page \n") 
 	
 		# we need a parser,Python built-in HTML parser is enough . 
 		soup=BeautifulSoup(resp.text,'html.parser')
@@ -24,13 +24,15 @@ def find_playlist_info(url):
 		songs = list()
 		for song in soup.find_all("a", "pl-video-title-link yt-uix-tile-link yt-uix-sessionlink spf-link"):
         		songs.append(song)
-
-		#print(songs)
-
+		
+		#contributors = list()
+		#for contributor in soup.find_all("a"):
+        		#if contributor.get('aria-label') != None : contributors.append(contributor.get('aria-label')[0:-7])
+		#print(contributors)
 		return songs
 
 	else: 
 		print("Error. Can't open the web page") 
 
-playlist_url = input("Enter the url of the youtube playlist: ")
-write_txt(find_playlist_info(playlist_url))
+playlistUrl = input("Enter the url of the youtube playlist: ")
+write_txt(find_playlist_info(playlistUrl))
